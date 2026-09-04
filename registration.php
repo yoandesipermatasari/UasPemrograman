f83d38c11cd1b0900470ed4b78a9af822924b10c
yoandesipermatasari <146821652+yoandesipermatasari@users.noreply.github.com>
2026-09-04 07:57:35 +0700

    Update README.md

---
+++ registration.php
@@ -0,0 +1,19 @@
+<?php
+include 'koneksi.php';
+
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $query = "INSERT INTO Akun (username, password) VALUES ('$username', '$password')";
+    $result = mysqli_query($conn, $query);
+
+    if ($result) {
+        header('Location: index.php');
+    } else {
+        header('Location: register.php?error=Registrasi gagal');
+    }
+} else {
+    header('Location: register.php');
+}
+?>
